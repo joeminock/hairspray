4bd9c53550bc657fdc2124f5d17739d26a9e7af0
Joe Minock <joe.minock@gmail.com>
2016-02-23 13:02:26 -0700

    updates to users controller, user views, routes, database schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160216035638) do
+ActiveRecord::Schema.define(version: 20160218041545) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20160216035638) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "fullname"
+    t.string   "phone_number"
+    t.text     "description"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
