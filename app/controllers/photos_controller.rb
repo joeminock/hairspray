3c6561cbd55ea6e65cac2bccb5426eaa9696b1e8
Joe Minock <joe.minock@gmail.com>
2016-02-29 19:25:46 -0700

    integrated images into stylist and added ajax for deleting images without refreshing screen.

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,12 @@
+class PhotosController < ApplicationController
+
+	def destroy
+		@photo = Photo.find(params[:id])
+		stylist = @photo.stylist	
+
+		@photo.destroy
+		@photos = Photo.where(stylist_id: stylist.id)
+
+		respond_to :js
+	end
+end
