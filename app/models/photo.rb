e528e3cdbff2ccfd83cc4aeb7f6e80cda72ea301
Joe Minock <joe.minock@gmail.com>
2016-04-28 13:17:58 -0600

    added file size validation and max file size of 6mb per image- May want to revisit file size later

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,5 +2,8 @@ class Photo < ActiveRecord::Base
   belongs_to :stylist
 
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates_attachment :image, presence: true,
+  						content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] },
+  						size: { in: 0..6144.kilobytes }	
+  									# content_type: /\Aimage\/.*\Z/
 end
